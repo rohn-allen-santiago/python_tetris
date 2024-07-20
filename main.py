183881cc891a6c79f059c50be96347fe33043753
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-07-20 16:03:21 +0100

    Added grid lines to the canvas

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ WINDOW_HEIGHT = 1000
 WINDOW_WIDTH = 1000
 CANVAS_HEIGHT = 1000
 CANVAS_WIDTH = 500
+BLOCK_WIDTH = 50
 
 # Create window and set dimensions
 pt = Tk()
@@ -20,6 +21,14 @@ f.pack()
 # Create canvas and set dimensions
 canvas = Canvas(f, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="gray")
 canvas.pack()
+
+# Draw grid lines on the canvas
+# Vertical lines
+for i in range(11):
+    canvas.create_line(i * 50, 0, i * 50, 1000)
+# Horizontal lines
+for i in range(21):
+    canvas.create_line(0, i * 50, 1000, i * 50)
 
 # Run main loop
 while True:

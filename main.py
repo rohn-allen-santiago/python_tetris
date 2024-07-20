b04d362ce8ce63cae0336b2c15cd7d4644b809ef
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-07-20 15:59:19 +0100

    Added canvas and frame to window

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from tkinter import *
 # Constants
 GRID_HEIGHT = 20
 GRID_WIDTH = 10
+WINDOW_HEIGHT = 1000
+WINDOW_WIDTH = 1000
 CANVAS_HEIGHT = 1000
-CANVAS_WIDTH = 1000
+CANVAS_WIDTH = 500
 
 # Create window and set dimensions
 pt = Tk()
 pt.title("Python Tetris")
-pt.geometry(str(CANVAS_WIDTH) + "x" + str(CANVAS_HEIGHT))
+pt.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
+
+# Create Frame and set dimensions
+f = Frame(pt, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg="white")
+f.pack()
+
+# Create canvas and set dimensions
+canvas = Canvas(f, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="gray")
+canvas.pack()
 
 # Run main loop
 while True:

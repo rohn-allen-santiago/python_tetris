f38b9d34a63ed541493a6e700449fd3a6e804a10
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-07-20 16:17:22 +0100

    Added method for drawing a block on teh canvas

--- block.py
+++ block.py
@@ -17,3 +17,11 @@ class Block:
         self.color = color
         self.speed = 1
 
+    # Draw the block on teh canvas
+    def draw(self):
+        x1 = self.x * BLOCK_WIDTH
+        y1 = self.y * BLOCK_WIDTH
+        x2 = x1 + BLOCK_WIDTH
+        y2 = y1 + BLOCK_WIDTH
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill=self.color)
+        return None

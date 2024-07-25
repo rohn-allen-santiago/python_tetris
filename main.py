879c63bede555158a892451564f9aee7bbba76d4
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-07-25 18:25:15 +0100

    Added listener to allow for user input and control

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from time import sleep
-from tkinter import *
-from block import *
+from mttkinter import *
+from piece import *
+from pynput.keyboard import Listener
+from math import *
 
 # Constants
-GRID_HEIGHT = 20
-GRID_WIDTH = 10
 WINDOW_HEIGHT = 1000
 WINDOW_WIDTH = 1000
 CANVAS_HEIGHT = 1000
 CANVAS_WIDTH = 500
 
+# Globals
+level = 1
+
 # Create window and set dimensions
-pt = Tk()
+pt = mtTkinter.Tk()
 pt.title("Python Tetris")
 pt.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
 
 # Create Frame and set dimensions
-f = Frame(pt, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg="white")
+f = mtTkinter.Frame(pt, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bg="white")
 f.pack()
 
 # Create canvas and set dimensions
-canvas = Canvas(f, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="gray")
+canvas = mtTkinter.Canvas(f, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="gray")
 canvas.pack()
 
 # Draw grid lines on the canvas
@@ -31,13 +34,42 @@ for i in range(11):
 for i in range(21):
     canvas.create_line(0, i * BLOCK_WIDTH, 1000, i * BLOCK_WIDTH)
 
+# Select action based on key press
+def input(key):
+    global currentPiece
+
+    # Move left
+    if key.char == 'a':
+        currentPiece.move_left()
+    # Move right
+    if key.char == 'd':
+        currentPiece.move_right()
+    # Rotate counterclockwise
+    if key.char == 'q':
+        currentPiece.rotate_ccw()
+    # Rotate clockwise
+    if key.char == 'e':
+        currentPiece.rotate_cw()
+    # Rotate 180 degrees
+    if key.char == 'w':
+        currentPiece.rotate_180()
+    # hard drop
+    if key.char == 'j':
+        currentPiece.hard_drop()
+    # Soft drop
+    # if key == key.from_char('s'):
+
+# Set up listener for registering key presses
+listener = Listener(on_press=input)
+listener.start()
+
 # Testing
-testBlock = Block(canvas, 1, 3, "red")
-testBlock.draw()
+testPiece = Piece(canvas, L_PIECE)
+testPiece.draw()
+currentPiece = testPiece
 
 # Run main loop
 while True:
     pt.update_idletasks()
     pt.update()
-    sleep(1)
-    testBlock.fall()
+    currentPiece.check_update(level)
